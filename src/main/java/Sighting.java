1e1389be1d8ac2c330f9562d9c80416e62aae728
vsoni3 <vsoni3@uncc.edu>
2017-01-10 20:19:11 -0500

    fixed parameters in Sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -46,7 +46,9 @@ public class Sighting {
     try(Connection con = DB.sql2o.open()) {
       String sql = "INSERT INTO sightings (animal_id, location, ranger_name) VALUES (:animal_id, :location, :ranger_name);";
       this.id = (int) con.createQuery(sql, true)
-       
+        .addParameter("animal_id", this.animal_id)
+        .addParameter("location", this.location)
+        .addParameter("ranger_name", this.ranger_name)
         .throwOnMappingFailure(false)
         .executeUpdate()
         .getKey();
